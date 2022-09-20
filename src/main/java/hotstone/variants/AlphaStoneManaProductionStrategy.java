825607344a569718ec6e0524995363e11afaa1a9
Timmi Nygaard Andersen <202105859@post.au.dk>
2022-09-20 10:55:13 +0200

    Refactored the name of our variant implementation classes

--- src/main/java/hotstone/variants/AlphaStoneManaProductionStrategy.java
+++ src/main/java/hotstone/variants/AlphaStoneManaProductionStrategy.java
@@ -2,7 +2,7 @@ package hotstone.variants;
 
 import hotstone.framework.strategies.ManaProductionStrategy;
 
-public class AlphaStoneManaProduction implements ManaProductionStrategy {
+public class AlphaStoneManaProductionStrategy implements ManaProductionStrategy {
 
     @Override
     public int calculateMana(int turnCounter) {
