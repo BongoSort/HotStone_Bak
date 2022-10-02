471499bf6202dc2fdeec4b6032151f9e97f580ad
Rune Schuster <runeschuster@gmail.com>
2022-10-02 16:54:49 +0200

    Testing success

--- src/main/java/hotstone/variants/DeltaStone/AlternatingDishDeckStrategy.java
+++ src/main/java/hotstone/variants/DeltaStone/AlternatingDishDeckStrategy.java
@@ -9,7 +9,13 @@ import hotstone.standard.StandardHotStoneCard;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class StandardDishDeckStrategy implements DeckStrategy {
+public class AlternatingDishDeckStrategy implements DeckStrategy {
+    //TODO vi skal helt ikke kunne smide AlphaStoneDeckStrategy ind som parameter her
+    private DeckStrategy dishDeckStrategy;
+
+    public AlternatingDishDeckStrategy(DeckStrategy dishDeckStrategy) {
+        this.dishDeckStrategy = dishDeckStrategy;
+    }
 
     @Override
     public ArrayList<Card> deckInitialization(Player who) {
@@ -17,19 +23,16 @@ public class StandardDishDeckStrategy implements DeckStrategy {
 
 
         for (int i = 0; i < 2; i++) {
-            deck.add(new StandardHotStoneCard(GameConstants.BROWN_RICE_CARD, who,1,1,2));
             deck.add(new StandardHotStoneCard(GameConstants.FRENCH_FRIES_CARD, who,1,2,1));
             deck.add(new StandardHotStoneCard(GameConstants.GREEN_SALAD_CARD, who,2,2,3));
-            deck.add(new StandardHotStoneCard(GameConstants.TOMATO_SALAD_CARD, who,2,3,2));
-            deck.add(new StandardHotStoneCard(GameConstants.POKE_BOWL_CARD, who,3,2,4));
             deck.add(new StandardHotStoneCard(GameConstants.PUMPKIN_SOUP_CARD, who,4,2,7));
-            deck.add(new StandardHotStoneCard(GameConstants.NOODLE_SOUP_CARD, who,4,5,3));
             deck.add(new StandardHotStoneCard(GameConstants.SPRING_ROLLS_CARD, who,5,3,7));
             deck.add(new StandardHotStoneCard(GameConstants.BAKED_SALMON_CARD, who,5,8,2));
-            deck.add(new StandardHotStoneCard(GameConstants.CHICKEN_CURRY_CARD, who,6,8,4));
-            deck.add(new StandardHotStoneCard(GameConstants.BEEF_BURGER_CARD, who,6,5,6));
             deck.add(new StandardHotStoneCard(GameConstants.FILET_MIGNON_CARD, who,7,9,5));
         }
+        deck.addAll(dishDeckStrategy.deckInitialization(who));
+
+
 
         Collections.shuffle(deck);
         Card firstCard = deck.stream().filter((c) -> c.getManaCost() == 1).findAny().orElse(null);
