d8927dbc6870b2b01b8eebfb3bf84a1cab72a9e7
MaiRicaplayz <mai.thoegersen@hotmail.com>
2022-11-02 11:52:41 +0100

    standardhotstoneGameTranscriber class is made

---
+++ src/main/java/hotstone/standard/StandardHotStoneGameTranscriber.java
@@ -0,0 +1,14 @@
+package hotstone.standard;
+
+import hotstone.framework.Game;
+
+public class StandardHotStoneGameTranscriber extends StandardHotStoneGameDecorator{
+
+    public StandardHotStoneGameTranscriber(Game game) {
+        super(game);
+    }
+
+    
+
+
+}
