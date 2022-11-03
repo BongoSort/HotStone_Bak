5d0038838f44427cb025ecca128628dbf9fe994f
Timmi Nygaard Andersen <202105859@post.au.dk>
2022-11-03 21:36:25 +0100

    worked on some stuff such as a role interface for Game, that were in our backlog, and fixed some dumb mistakes we made earlier

--- src/test/java/hotstone/standard/TestPersonalizedDeck.java
+++ src/test/java/hotstone/standard/TestPersonalizedDeck.java
@@ -18,11 +18,21 @@ public class TestPersonalizedDeck {
     private DeckStrategy personalizedDeckStrategy;
     private PersonalDeckReader personalDeckReader;
 
-    @Test
-    public void WhenPersonalizedDeckIsAnimalDeckItContainsTwoOfEachCardFromTheDeck() {
+    @BeforeEach
+    public void setUp() {
         personalDeckReader = new PersonalDeckReader("animaldeck.json");
         personalizedDeckStrategy = new PersonalizedDeckStrategy(personalDeckReader);
+    }
 
+
+    @Test
+    public void aPersonalizedDeckSizeIs16() {
+        ArrayList<Card> deck = personalizedDeckStrategy.deckInitialization(Player.FINDUS);
+        assertThat(deck.size(), is(16));
+    }
+
+    @Test
+    public void WhenPersonalizedDeckIsAnimalDeckItContainsTwoOfEachCardFromTheDeck() {
         ArrayList<Card> deck = personalizedDeckStrategy.deckInitialization(Player.FINDUS);
         int res = (int) deck.stream().filter(card -> card.getName().equals("Fish")).count();
         assertThat(res, is(2));
@@ -47,5 +57,56 @@ public class TestPersonalizedDeck {
 
         res = (int) deck.stream().filter(card -> card.getName().equals("Sheep")).count();
         assertThat(res, is(2));
+    }
+
+    @Test
+    public void WhenPersonalizedDeckIsNotAnimalDeckItContainsTwoOfEachCardFromTheList() {
+        personalDeckReader = new PersonalDeckReader("norsegoddeck.json");
+        personalizedDeckStrategy = new PersonalizedDeckStrategy(personalDeckReader);
+
+        ArrayList<Card> deck = personalizedDeckStrategy.deckInitialization(Player.FINDUS);
+        int res = (int) deck.stream().filter(card -> card.getName().equals("Loki")).count();
+        assertThat(res, is(2));
+
+        res = (int) deck.stream().filter(card -> card.getName().equals("Heimdallur")).count();
+        assertThat(res, is(2));
+
+        res = (int) deck.stream().filter(card -> card.getName().equals("Hel")).count();
+        assertThat(res, is(2));
+
+        res = (int) deck.stream().filter(card -> card.getName().equals("Sif")).count();
+        assertThat(res, is(2));
+
+        res = (int) deck.stream().filter(card -> card.getName().equals("Baldur")).count();
+        assertThat(res, is(2));
+
+        res = (int) deck.stream().filter(card -> card.getName().equals("Freyja")).count();
+        assertThat(res, is(2));
+
+        res = (int) deck.stream().filter(card -> card.getName().equals("Thor")).count();
+        assertThat(res, is(2));
+
+        res = (int) deck.stream().filter(card -> card.getName().equals("Odin")).count();
+        assertThat(res, is(2));
+    }
+
+    @Test
+    public void afterDeckInitializationFirstCardInDeckShouldHaveManaCost1() {
+        ArrayList<Card> deck = personalizedDeckStrategy.deckInitialization(Player.FINDUS);
+        assertThat(deck.get(0).getManaCost(), is(1));
+    }
+
+
+    @Test
+    public void afterDeckInitializationFirstCardInDeckShouldHaveManaCost2OrLower() {
+        ArrayList<Card> deck = personalizedDeckStrategy.deckInitialization(Player.FINDUS);
+        assertThat(deck.get(1).getManaCost() <= 2, is(true));
+    }
+
+
+    @Test
+    public void afterDeckInitializationFirstCardInDeckShouldHaveManaCost4OrLower() {
+        ArrayList<Card> deck = personalizedDeckStrategy.deckInitialization(Player.FINDUS);
+        assertThat(deck.get(2).getManaCost() <= 4, is(true));
     }
 }
