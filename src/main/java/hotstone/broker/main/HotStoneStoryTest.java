08d75aef775e43341a6e316d55b8d66b7c8fb163
Rune Schuster <102947520+BongoSort@users.noreply.github.com>
2022-11-21 18:42:52 +0100

    Added a few prints to storyTest

--- src/main/java/hotstone/broker/main/HotStoneStoryTest.java
+++ src/main/java/hotstone/broker/main/HotStoneStoryTest.java
@@ -49,24 +49,31 @@ public class HotStoneStoryTest {
 
   private void testSimpleMethods(Game game) {
     System.out.println("=== Testing pass-by-value methods of Game ===");
-    System.out.println(" --> Game turnNumber           " + game.getTurnNumber());
-    System.out.println(" --> Game playerInTurn         " + game.getPlayerInTurn());
-    System.out.println(" --> Game winner               " + game.getWinner());
-    System.out.println(" --> Game FindusDeckSize       "  + game.getDeckSize(Player.FINDUS));
-    System.out.println(" --> Game PeddersenDeckSize    " + game.getWinner());
-    System.out.println(" --> Game FindusHandSize       " + game.getHandSize(Player.FINDUS));
-    System.out.println(" --> Game PeddersenHandSize    " + game.getHandSize(Player.PEDDERSEN));
-    System.out.println(" --> Game FindusFieldSize      " + game.getFieldSize(Player.FINDUS));
-    System.out.println(" --> Game PeddersenFieldSize   " + game.getFieldSize(Player.PEDDERSEN));
-    System.out.println(" --> Game FindusPlaysCard      " + game.playCard(Player.FINDUS, new StubCardForBroker()));
-    System.out.println(" --> Game PeddersenPlaysCard   " + game.playCard(Player.PEDDERSEN, new StubCardForBroker()));
-    System.out.println(" --> Game FindusAttacksCard    " + game.attackCard(Player.FINDUS, new StubCardForBroker(), new StubCardForBroker()));
-    System.out.println(" --> Game PeddersenAttacksCard " + game.attackCard(Player.PEDDERSEN, new StubCardForBroker(), new StubCardForBroker()));
-    System.out.println(" --> Game FindusAttacksHero    " + game.attackHero(Player.FINDUS, new StubCardForBroker()));
-    System.out.println(" --> Game PeddersenAttacksHero " + game.attackHero(Player.PEDDERSEN, new StubCardForBroker()));
-    System.out.println(" --> Game FindusUsesPower      " + game.usePower(Player.FINDUS));
-    System.out.println(" --> Game PeddersenUsesPower   " + game.usePower(Player.PEDDERSEN));
+    System.out.println(" --> Game turnNumber            " + game.getTurnNumber());
+    System.out.println(" --> Game playerInTurn          " + game.getPlayerInTurn());
+    System.out.println(" --> Game winner                " + game.getWinner());
+    System.out.println(" --> Game FindusDeckSize        " + game.getDeckSize(Player.FINDUS));
+    System.out.println(" --> Game PeddersenDeckSize     " + game.getWinner());
+    System.out.println(" --> Game FindusHandSize        " + game.getHandSize(Player.FINDUS));
+    System.out.println(" --> Game PeddersenHandSize     " + game.getHandSize(Player.PEDDERSEN));
+    System.out.println(" --> Game FindusFieldSize       " + game.getFieldSize(Player.FINDUS));
+    System.out.println(" --> Game PeddersenFieldSize    " + game.getFieldSize(Player.PEDDERSEN));
+    System.out.println(" --> Game FindusPlaysCard       " + game.playCard(Player.FINDUS, new StubCardForBroker()));
+    System.out.println(" --> Game PeddersenPlaysCard    " + game.playCard(Player.PEDDERSEN, new StubCardForBroker()));
+    System.out.println(" --> Game FindusAttacksCard     " + game.attackCard(Player.FINDUS, new StubCardForBroker(), new StubCardForBroker()));
+    System.out.println(" --> Game PeddersenAttacksCard  " + game.attackCard(Player.PEDDERSEN, new StubCardForBroker(), new StubCardForBroker()));
+    System.out.println(" --> Game FindusAttacksHero     " + game.attackHero(Player.FINDUS, new StubCardForBroker()));
+    System.out.println(" --> Game PeddersenAttacksHero  " + game.attackHero(Player.PEDDERSEN, new StubCardForBroker()));
+    System.out.println(" --> Game FindusUsesPower       " + game.usePower(Player.FINDUS));
+    System.out.println(" --> Game PeddersenUsesPower    " + game.usePower(Player.PEDDERSEN));
     // TODO - add calls to the rest of the implemented methods
+    System.out.println(" --> Game FindusHand            " + game.getHand(Player.FINDUS));
+    System.out.println(" --> Game PeddersendHand        " + game.getHand(Player.PEDDERSEN));
+    System.out.println(" --> Game FindusField           " + game.getField(Player.FINDUS));
+    System.out.println(" --> Game PeddersendField       " + game.getField(Player.PEDDERSEN));
+    System.out.println(" --> Game PeddersendField       " + game.getField(Player.PEDDERSEN));
+    System.out.println(" --> Game FindusCardInField     " + game.getCardInField(Player.FINDUS, 0)); //TODO? index ok?
+    System.out.println(" --> Game PeddersendCardInField " + game.getCardInField(Player.PEDDERSEN, 0)); //TODO? index ok?
     System.out.println("=== End ===");
   }
 }
